84ba6284ddb0e6ffb35215e567220fc2ca4551f2
凌 松本 <150272@jc-21.jp>
2017-12-01 16:55:45 +0900

    い

--- BMI/BMI/MainPage.xaml.cs
+++ BMI/BMI/MainPage.xaml.cs
@@ -15,9 +15,43 @@ namespace BMI
         }
         private void BMI_Button_Ciick(Object sender,EventArgs e)
         {
-            double dheight = double.Parse(height.Text);
+            double dHeight = double.Parse(height.Text);
+            double dWeight = double.Parse(weight.Text);
 
-            DisplayAlert("BMI", dheight+":太りすぎ", "OK");
+            double dBMI = dWeight / dHeight / dHeight;
+            string message;
+
+            if(dBMI >= 30.0 )
+            {
+                message = "肥満";
+            }
+
+            else if(dBMI >= 25.0)
+            {
+                message = "前肥満";
+            }
+
+            else if (dBMI >= 18.5)
+            {
+                message = "普通体重";
+            }
+
+            else if (dBMI >= 17.0)
+            {
+                message = "痩せぎみ";
+            }
+
+            else if (dBMI >= 16.0)
+            {
+                message = "痩せ";
+            }
+
+            else 
+            {
+                message = "痩せすぎ";
+            }
+
+            DisplayAlert("BMI", dBMI+":"+message, "OK");
         }
     }
 }
